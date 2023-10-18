487ffa6b1f55ffe56a01bba3daa558231c62b98c
Taliesynth <taliesynth@gmail.com>
2023-10-18 03:52:43 -0400

    moved search bar and started merging compendium

--- src/components/search layout.js
+++ src/components/search layout.js
@@ -1,70 +1,74 @@
 import { useState } from 'react'
 import React from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
-import { search_button, search_container} from './search layout.module.css'
+import { search_button, search_container} from './layout.module.css'
 
 const SearchLayout = () => {
-    const [searchValue, setSearchValue] = useState('');
-    const [searchResult, setSearchResult] = useState([]);
-    const data = useStaticQuery(graphql`
-      query {
-        allMdx {
-          nodes {
-            frontmatter {
-              title
-              slug
-            }
+  const [searchValue, setSearchValue] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx(sort: { frontmatter: { title: ASC }}) {
+        nodes {
+          frontmatter {
+            title
+            slug
+            arcana
           }
+          id
+          excerpt
         }
       }
-    `);
+    }
+  `);
   
-    const handleSearch = () => {
+  const handleSearch = () => {
 
-      const filteredNodes = data.allMdx.nodes.filter((node) =>
-        node.frontmatter.title.toLowerCase().includes(searchValue.toLowerCase())
-      );
-  
-      setSearchResult(filteredNodes);
-    };
-  
-    const handleChange = (event) => {
+    const filteredNodes = data.allMdx.nodes.filter((node) =>
+      node.frontmatter.title.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    
+    setSearchResult(filteredNodes);
+  };
+    
+  const handleChange = (event) => {
 
-      setSearchValue(event.target.value);
-    };
-  
-    return (
+    setSearchValue(event.target.value);
+  };
+    
+  return (
+    <div>
+      <div className={search_container}>
+        <input
+          type="text"
+          id="searchInput"
+          placeholder="Search by keyword..."
+          onChange={handleChange}
+        />
+        <button
+          className={search_button}
+          id="searchButton"
+          onClick={handleSearch}
+        >
+          Search
+        </button>
+      </div>
       <div>
-        <div className={search_container}>
-          <input
-            type="text"
-            id="searchInput"
-            placeholder="Search by keyword..."
-            onChange={handleChange} 
-          />
-          <button
-            className={search_button}
-            id="searchButton"
-            onClick={handleSearch}
-          >
-            Search
-          </button>
-        </div>
         <div>
-          <div>
-            {searchResult.map((node) => (
-              <div>
-                <h2>
-                  <Link to={`../compendium/${node.frontmatter.slug}`}>
-                    {node.frontmatter.title}
-                  </Link>
-                </h2>
-              </div>
-            ))}
-          </div>
+          {searchResult.map((node) => (
+            <div>
+              <h2>
+                <Link to={`../compendium/${node.frontmatter.slug}`}>
+                  {node.frontmatter.title}
+                </Link>
+                <p>Arcana: {node.frontmatter.arcana}</p>
+              </h2>
+            </div>
+          ))}
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
   
-  export default SearchLayout;
+export default SearchLayout;
